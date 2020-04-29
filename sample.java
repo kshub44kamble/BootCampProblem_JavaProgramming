fb6e5ac52efdbdb1447555ea98c3e3bc370ac0cb
shubhamkamble <kshub44kamble@gmail.com>
2020-04-29 14:22:47 +0530

    added: sample program

---
+++ sample.java
@@ -0,0 +1,13 @@
+/**
+  *class sample print hello msg
+  *@Author: Shubham
+*/
+
+public class sample {
+	/**
+	  *main method param args
+	*/
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
