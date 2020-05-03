14026ae2e1fda412b955c73712fbcf4c877410e6
shubhamkamble <kshub44kamble@gmail.com>
2020-05-03 16:10:54 +0530

    added: Vector Collection Problem

---
+++ JavaCollections/VectorCollection.java
@@ -0,0 +1,22 @@
+import java.util.*;
+/**
+  *class Vector Collection
+  *@author: shubhamkamble
+*/
+public class VectorCollection{
+/**
+  *main method
+  *@param args args
+*/
+public static void main(String args[]){
+	Vector<String> v=new Vector<String>();
+	v.add("Ayush");
+	v.add("Amit");
+	v.add("Ashish");
+	v.add("Garima");
+	Iterator<String> itr=v.iterator();
+		while(itr.hasNext()){
+			System.out.println(itr.next());
+		}
+	}
+}
